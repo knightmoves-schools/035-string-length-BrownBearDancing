0459d7a318b69e9b2ca7041769ee6f7458e77ee1
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-23 14:15:29 -0500

    what am I missing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let quote = "...ask not what your country can do for you — ask what you can do
 
 let quoteLength= quote.length;
 let twentySecondLetter= quote[21];
-let output= 'this is my string with ${quoteLength} and also [${twentySecondLetter}]';
+let output= "this is my string with ${quoteLength} and also [${twentySecondLetter}]";
 
 document.getElementById("result").innerHTML= output;
 
