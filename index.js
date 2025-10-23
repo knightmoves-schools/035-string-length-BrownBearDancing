511336f6e50e4635cee89a97753e2cefce484f29
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2025-10-23 14:07:14 -0500

    try this one

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 let quoteLength= quote.length;
-let twentySecondLetter= quote [21];
-let output= quote[twentySecondLetter];
+let twentySecondLetter= quote[21];
+let output= 'this is my string with ${quoteLength} and also{$[twentySecondLetter]}';
 
-document.getElementById("output").innerHTML= output;
+document.getElementById("result").innerHTML= output;
 
 
 //Add code below
